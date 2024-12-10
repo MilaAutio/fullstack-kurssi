b44bf68c47eb365658f538edbd8cce9449250f81
Mila Autio <mila.autio@hotmail.com>
2024-12-10 18:32:22 +0200

    Complited exercises 6.3 - 6.8

---
+++ Osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux'
+import { createNote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+
+    const dispatch = useDispatch()
+
+    const addNote = (event) => {
+        event.preventDefault()
+        const content = event.target.note.value
+        event.target.note.value = ''
+        console.log(createNote(content))
+        dispatch(createNote(content))
+      }
+
+    return (
+      <div>
+        <h2>create new</h2>
+        <form onSubmit={addNote}>
+            <div><input name="note" /></div>
+            <button>create</button>
+        </form>
+      </div>
+    )
+}
+
+export default AnecdoteForm
