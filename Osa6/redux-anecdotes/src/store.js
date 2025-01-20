3efe71a957a1d8c791692afcabaee3c4bc5c6ea2
Mila Autio <mila.autio@hotmail.com>
2025-01-20 19:43:17 +0200

    Exercise 6.14 - Get anecdotes from backend

--- Osa6/redux-anecdotes/src/store.js
+++ Osa6/redux-anecdotes/src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import anecdoteSlice from './reducers/anecdoteReducer'
 import filterSlice from './reducers/filterReducer'
 import notificationSlice from './reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 export const store = configureStore({
     reducer: {
@@ -9,4 +10,8 @@ export const store = configureStore({
         filter: filterSlice.reducer,
         notification: notificationSlice.reducer
     }
+})
+
+anecdoteService.getAll().then(anecdotes => {
+    store.dispatch(anecdoteSlice.actions.setAnecdotes(anecdotes))
 })
