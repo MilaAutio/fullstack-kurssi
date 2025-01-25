9994eb6dad0008ccbf40753acae61261907043f5
Mila Autio <mila.autio@hotmail.com>
2025-01-25 11:40:46 +0200

    Exercse 6.21 - Add new anecdotes to database

--- Osa6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ Osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+
 const AnecdoteForm = () => {
+
+  const queryClient =  useQueryClient()
+
+  const newNoteMutation = useMutation({
+      mutationFn: (newAnecdote) => {
+        axios.post('http://localhost:3001/anecdotes', newAnecdote).then(res => res.data)
+      },
+      onSuccess: (newAnecdote) => {
+        queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+      }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    newNoteMutation.mutate({ content, votes: 0 })
+  }
 
   return (
     <div>
